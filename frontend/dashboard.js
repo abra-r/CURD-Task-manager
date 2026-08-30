4f04677ae6fa14ee257a257fa1b63aff24833755
Abrar Amin <abrarabrar4766@gmail.com>
2026-08-31 01:59:41 +0600

    Edit task and delete task interface added

--- frontend/dashboard.js
+++ frontend/dashboard.js
@@ -2,10 +2,14 @@
 const API_BASE='http://localhost:3001';
 const tasks=document.getElementById('tasks');
 const errorMessage=document.getElementById('error-message');
+const listContainer=document.getElementById('list-container');
+const taskForm=document.getElementById('create-task');
+const token=localStorage.getItem('token');
+
+const globalMessage=document.getElementById('global-message');
 
 const getAllTasks= async ()=>{
     try {
-        const token=localStorage.getItem('token');
         if(!token)
         {
             
@@ -25,7 +29,19 @@ const getAllTasks= async ()=>{
             errorMessage.textContent=data.error||"Something worng";
             return;
         }
-        tasks.textContent=JSON.stringify(data);
+        console.log(typeof data);
+        listContainer.innerHTML=data.tasks.map(task=>
+            `<li>
+            <h4>Title: ${task.title}</h4>
+            <span><b>Task id:</b> ${task._id},</span> 
+            <span><b>Details:</b> ${task.description===undefined?'N/A ':task.description}</span>,
+            
+            <span><b>Status: </b> ${task.completed? 'Completed':'Pending'}</span>  
+            
+            </li>`
+
+        ).join(' ');
+
     }
     catch(err)
     {
@@ -33,4 +49,65 @@ const getAllTasks= async ()=>{
         console.log(err);
     }
 }
+taskForm.addEventListener('submit', async (event)=>{
+    event.preventDefault();
+    const taskTitle=document.getElementById('new-title').value.trim();
+    const newDetails=document.getElementById('new-details').value.trim();
+    const taskCreateMessage=document.getElementById('task-create-message');
+    if(taskTitle===''){
+        taskCreateMessage.textContent='Title can not be empty';
+
+    }
+    
+
+    
+    const response=await fetch(`${API_BASE}/tasks/create`,{
+        method:'POST',
+        headers:{
+            'Authorization':`Bearer ${token}`,
+            'Content-Type':'application/json'
+        },
+        body:JSON.stringify({'title':taskTitle,'description':newDetails})
+
+    })
+    if(response.ok)
+    {
+        window.location.reload();
+        taskCreateMessage.textContent=`Task ${title} created successfully`;
+
+    }
+    else{
+        taskCreateMessage.textContent=`Something went wrong.}`
+    }
+})
+
+const deleteTask=document.getElementById('delete-task');
+deleteTask.addEventListener('submit',async (event)=>
+{
+    event.preventDefault();
+    const taskId=document.getElementById('task-id').value.trim();
+    if(taskId===''){
+        globalMessage.textContent='Enter valid id';
+        
+    }
+    try 
+    {
+        const respons=await fetch(`${API_BASE}/tasks/delete/${taskId}`,{
+            method:'DELETE',
+            headers:{
+                'Authorization':`Bearer ${token}`
+            }
+
+        })
+        globalMessage.textContent='Taske deleted successfully';
+        document.location.reload();
+        
+    }
+    catch (err){
+        console.log(`Sometging Werong. Details`);
+    }
+    
+
+
+})
 getAllTasks();
